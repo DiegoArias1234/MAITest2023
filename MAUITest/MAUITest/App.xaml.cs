06fd3b9e22fbbd2890ab9f5bb6e76e604c4ede9c
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-04-16 12:38:07 -0500

    FlylaYout demo

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -5,11 +5,13 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-        var navPage = new NavigationPage(new ContentPageDemo());
-        navPage.BarBackgroundColor = Colors.Chocolate;
-        navPage.BarTextColor = Colors.White;
-        navPage.Title = "DEMO MAUI";
-        MainPage = navPage;
+        //var navPage = new NavigationPage(new ContentPageDemo());
+        //navPage.BarBackgroundColor = Colors.Chocolate;
+        //navPage.BarTextColor = Colors.White;
+        //navPage.Title = "DEMO MAUI";
+        //MainPage = navPage;
+
+        MainPage = new NavigationPage(new FlyoutPageDemo());
 
     }
 }
