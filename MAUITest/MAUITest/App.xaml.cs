00d15c18623f02c97027a41cc6145eb9394e77a5
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-04-16 12:07:48 -0500

    Content page Demo

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -5,7 +5,11 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+        var navPage = new NavigationPage(new ContentPageDemo());
+        navPage.BarBackgroundColor = Colors.Chocolate;
+        navPage.BarTextColor = Colors.White;
+        navPage.Title = "DEMO MAUI";
+        MainPage = navPage;
 
-		MainPage = new AppShell();
-	}
+    }
 }
