3b659e00b5ea2e06e4435239f4f415b65cbd74ae
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-04-23 11:22:49 -0500

    Finish controls to show information

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -11,7 +11,7 @@ public partial class App : Application
         //navPage.Title = "DEMO MAUI";
         //MainPage = navPage;
 
-        MainPage = new NavigationPage(new FlyoutPageDemo());
+        MainPage = new NavigationPage(new PresentationControlsDemo());
 
     }
 }
